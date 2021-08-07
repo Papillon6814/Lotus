8a97205f226c18e8d5f1c6fa04a0c9f1c153d294
Papillon6814 <so1rou.k@gmail.com>
2021-08-07 21:35:10 +0900

    learning: types

--- learn/polling_app/voter/vue/src/views/Types.vue
+++ learn/polling_app/voter/vue/src/views/Types.vue
@@ -2,6 +2,7 @@
 	<div>
 		<div class="container">
 			<!-- this line is used by starport scaffolding # 4 -->
+		<SpType modulePath="papillon6814.voter.voter" moduleType="Poll"  />
 		</div>
 	</div>
 </template>
