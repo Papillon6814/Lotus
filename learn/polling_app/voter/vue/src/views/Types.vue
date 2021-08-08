9c22986839b746d0c0e44b8a5759ddba738e0ef0
Papillon6814 <so1rou.k@gmail.com>
2021-08-09 01:12:41 +0900

    feat: web ui

--- learn/polling_app/voter/vue/src/views/Types.vue
+++ learn/polling_app/voter/vue/src/views/Types.vue
@@ -2,14 +2,20 @@
 	<div>
 		<div class="container">
 			<!-- this line is used by starport scaffolding # 4 -->
-		<SpType modulePath="papillon6814.voter.voter" moduleType="Vote"  />
-		<SpType modulePath="papillon6814.voter.voter" moduleType="Poll"  />
+			<!-- <SpType modulePath="papillon6814.voter.voter" moduleType="Vote"  />
+			<SpType modulePath="papillon6814.voter.voter" moduleType="Poll"  /> -->
+			<PollForm />
+			<PollList />
 		</div>
 	</div>
 </template>
 
 <script>
+import PollForm from "../components/PollForm"
+import PollList from "../components/PollList"
+
 export default {
-	name: 'Types'
+	name: 'Types',
+	components: { PollForm, PollList }
 }
 </script>
